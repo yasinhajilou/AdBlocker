09b34982fc92ac8d781f624c9030b611ccd577f9
Yasin Hajilou <41325533+yasinhajilou@users.noreply.github.com>
2021-05-03 21:06:46 +0430

    Initial commit

---
+++ app/src/main/java/com/yasinhajilou/disconnector/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.yasinhajilou.disconnector
+
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            val controller = window.insetsController
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            // Hide the status bar.
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            actionBar?.hide()
+        }
+
+
+    }
+}
