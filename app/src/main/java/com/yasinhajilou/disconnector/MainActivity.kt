b416471cba119926804afb99f53cb2f785187d8c
Yasin Hajilou <41325533+yasinhajilou@users.noreply.github.com>
2021-05-03 21:17:35 +0430

    Modified: color and xml view

--- app/src/main/java/com/yasinhajilou/disconnector/MainActivity.kt
+++ app/src/main/java/com/yasinhajilou/disconnector/MainActivity.kt
@@ -12,22 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-            val controller = window.insetsController
-            if (controller != null) {
-                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                controller.systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            // Hide the status bar.
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-            // Remember that you should never show the action bar if the
-            // status bar is hidden, so hide that too if necessary.
-            actionBar?.hide()
-        }
-
-
     }
 }
