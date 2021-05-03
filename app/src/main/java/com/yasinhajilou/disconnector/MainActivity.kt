c10d021940ce487833c795f44281c83009ce4034
Yasin Hajilou <41325533+yasinhajilou@users.noreply.github.com>
2021-05-03 22:04:55 +0430

    added: animation handler

--- app/src/main/java/com/yasinhajilou/disconnector/MainActivity.kt
+++ app/src/main/java/com/yasinhajilou/disconnector/MainActivity.kt
@@ -10,13 +10,29 @@ import com.yasinhajilou.disconnector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var activityMainBinding: ActivityMainBinding
+    var vpnConnection = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
-        activityMainBinding.ivSwitch.setOnClickListener {
+        vpnConnection = VpnUtil.isVpnConnected(this)
 
+        if (vpnConnection) {
+            activityMainBinding.tvVpnStatus.text = getString(R.string.vpn_on)
+            activityMainBinding.rippleAnimation.playAnimation()
+        } else {
+            activityMainBinding.tvVpnStatus.text = getString(R.string.vpn_off)
+            finishAnimation()
         }
+
+        activityMainBinding.ivSwitch.setOnClickListener {
+            finishAnimation()
+        }
+    }
+
+    fun finishAnimation() {
+        activityMainBinding.rippleAnimation.progress = 0f
+        activityMainBinding.rippleAnimation.pauseAnimation()
     }
 }
